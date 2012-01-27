53cd179c3bb3502e27815e83fbb499c67ba280de
Sciss <contact@sciss.de>
2012-01-27 21:30:14 +0000

    fix for Silent removal

--- src/main/scala/de/sciss/synth/ugens/MatrixOut.scala
+++ src/main/scala/de/sciss/synth/ugens/MatrixOut.scala
@@ -27,7 +27,7 @@ package de.sciss.synth
 package ugens
 
 import de.sciss.kontur.util.Matrix2D
-import ugen.{Mix, Silent}
+import ugen.Mix
 
 /**
  * Temporary work around the fact the Out.ar is not checking against scalar values
@@ -42,19 +42,20 @@ final case class MatrixOut( in: GE, m: Matrix2D[ Float ]) extends GE.Lazy {
       val ins     = _in.outputs
       val numIns  = ins.size
       require( numIns == m.numRows )
-      val outs0 = Seq.tabulate[ GE ]( m.numRows )( outCh => Mix.mono( ins * Seq.tabulate( numIns )( inCh => m( inCh, outCh ))).expand )
-      val numZeroes = outs0.count {
-         case Constant( 0 ) => true
-         case _ => false
-      }
-      val outs = if( numZeroes > 0 ) {
-         val sil = Silent.ar( numZeroes ).expand.outputs
-         var idx = 0
-         outs0.map {
-            case Constant( 0 ) => val res = sil( idx ); idx += 1; res
-            case x => x
-         }
-      } else outs0
+      val outs = Seq.tabulate[ GE ]( m.numRows )( outCh => Mix.mono( ins * Seq.tabulate( numIns )( inCh => m( inCh, outCh ))).expand )
+//      val numZeroes = outs0.count {
+//         case Constant( 0 ) => true
+//         case _ => false
+//      }
+//      val outs = if( numZeroes > 0 ) {
+//         val sil = Silent.ar( numZeroes ).expand.outputs
+//         var idx = 0
+//         outs0.map {
+//            case Constant( 0 ) => val res = sil( idx ); idx += 1; res
+//            case x => x
+//         }
+//      } else outs0
+//      val outs = UGenHelper.replaceZeroesWithSilence( outs0 )
 
       (outs: GE).expand
 //
